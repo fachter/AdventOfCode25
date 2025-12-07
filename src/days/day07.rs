5d2d0588d4e7dde78f688cca0702fdd857a61cbc
Felix Achter <felix.achter.it@gmail.com>
2025-12-07 19:51:17 +0100

    second part day 07

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -14,28 +14,31 @@ fn day07(file_name: &str) {
 
     let first_line = lines.first().expect("Has to have a start line");
     let start_beem = first_line.find('S').expect("Has to have a start beam");
-    let mut beams: HashSet<usize> = HashSet::from([start_beem]);
+    let mut beams: HashMap<usize, u128> = HashMap::from([(start_beem, 1)]);
     let mut counter = 0;
+    // let mut timelines = 1;
     for line in lines {
         let previous_beams = beams.clone();
         beams.clear();
         let chars: Vec<char> = line.chars().collect();
-        for beam in &previous_beams {
+        let mut visited: HashSet<usize> = HashSet::new();
+        for (beam, beam_count) in &previous_beams {
             match chars[*beam] {
                 '^' => {
-                    beams.insert(beam - 1);
-                    beams.insert(beam + 1);
+                    beams.insert(beam - 1, beams.get(&(beam - 1)).unwrap_or(&0) + beam_count);
+                    beams.insert(beam + 1, beams.get(&(beam + 1)).unwrap_or(&0) + beam_count);
+
                     counter += 1;
                 }
                 _ => {
-                    beams.insert(*beam);
+                    beams.insert(*beam, beams.get(&beam).unwrap_or(&0) + beam_count);
                 }
             }
+            visited.insert(*beam);
         }
-        // println!("{:?}", beams);
-        // println!("{:?}", previous_beams);
-        // println!("{}", line);
     }
 
+    let timelines: u128 = beams.values().sum();
     println!("Counter: {}", counter);
+    println!("Timelines: {}", timelines);
 }
