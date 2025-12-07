119cbbd15155bdb64a240ad36ea655430b287c48
Felix Achter <felix.achter.it@gmail.com>
2025-12-07 11:30:24 +0100

    day 03 with 12 batteries

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
-use std::fs;
+use std::{cmp::max, collections::HashMap, fs};
 
 fn main() {
     let lines = read_lines("03");
 
-    let mut sum = 0;
+    let mut sum: u128 = 0;
+    let n_batteries = 12usize;
     for line in lines {
-        let mut largest = 0;
-        let mut second_largest = 0;
+        let mut max_values: HashMap<u32, u128> = HashMap::new();
+        for _ in 0..n_batteries {
+            max_values.insert(0, 0 as u128);
+        }
         println!("{}", line);
 
         let chars: Vec<char> = line.chars().collect();
-        for char_value in &chars[..chars.len() - 1] {
-            let number = char_value.to_digit(10).unwrap();
-            if number > largest {
-                largest = number;
-                second_largest = 0;
-            } else if number > second_largest {
-                second_largest = number;
-            }
-        }
-        let last_char = chars[chars.len() - 1].to_digit(10).unwrap();
-        if last_char > second_largest {
-            second_largest = last_char;
+        let char_count = chars.len();
+        for (i, char_value) in (&chars).iter().enumerate() {
+            let number = char_value.to_digit(10).unwrap() as u128;
+            update_max_values(
+                &mut max_values,
+                number,
+                n_batteries,
+                max(0, (n_batteries as i32) - ((char_count as i32) - (i as i32))) as u32,
+            );
         }
 
-        println!("Combined Number: {}{}", largest, second_largest);
-        let value = largest * 10 + second_largest;
+        // println!("Max Values: {:?}", max_values);
+        let mut value: u128 = 0;
+        for (key, val) in &max_values {
+            let power_value = (n_batteries as u32 - 1) - key;
+            value += val * 10u128.pow(power_value);
+        }
+        // println!("Value: {}", value);
+
+        // println!("Combined Number: {}{}", largest, second_largest);
+        // let value = largest * 10 + second_largest;
         sum += value;
     }
 
@@ -39,4 +47,24 @@ fn read_lines(file_name: &str) -> Vec<String> {
         .lines()
         .map(|x| x.to_string())
         .collect::<Vec<String>>()
+}
+
+fn update_max_values(
+    max_values: &mut HashMap<u32, u128>,
+    number: u128,
+    n_batteries: usize,
+    index: u32,
+) {
+    if index >= (n_batteries as u32) {
+        return;
+    }
+    let current = max_values.get(&index).copied().unwrap_or(0);
+    if number > current {
+        max_values.insert(index, number);
+        for i in (index + 1)..(n_batteries as u32) {
+            max_values.insert(i, 0);
+        }
+    } else {
+        update_max_values(max_values, number, n_batteries, index + 1);
+    }
 }
